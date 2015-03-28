e5e7dec3e653cdfc9ae4c1c5770743c3cd768e81
Ruto8 <pawel-rutkowski@hotmail.com>
2015-03-28 08:07:08 +0100

    Convert Api::UsersController to module Api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,22 +1,24 @@
-class Api::UsersController < ApplicationController
+module Api
+  class UsersController < ApplicationController
 
-  def index
-    render json: User.sooners(current_user_data['uid'])
-  end
-
-  def update
-    user = User.find_by(sso_id: current_user_data['uid'])
-    user.update!(user_params)
-
-    render json: user
-  end
-
-  def show
-    render json: User.find_by(sso_id: current_user_data['uid'])
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :email)
+    def index
+      render json: User.sooners(current_user_data['uid'])
     end
+
+    def update
+      user = User.find_by(sso_id: current_user_data['uid'])
+      user.update!(user_params)
+
+      render json: user
+    end
+
+    def show
+      render json: User.find_by(sso_id: current_user_data['uid'])
+    end
+
+    private
+      def user_params
+        params.require(:user).permit(:name, :email)
+      end
+  end
 end
