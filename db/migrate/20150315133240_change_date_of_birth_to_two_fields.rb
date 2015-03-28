2aeb4e3a214ceefa28faac38043728396136ff11
Ruto8 <pawel-rutkowski@hotmail.com>
2015-03-28 07:53:26 +0100

    Add :date to removal of :date_of_birth so it can be reversible

--- db/migrate/20150315133240_change_date_of_birth_to_two_fields.rb
+++ db/migrate/20150315133240_change_date_of_birth_to_two_fields.rb
@@ -2,6 +2,6 @@ class ChangeDateOfBirthToTwoFields < ActiveRecord::Migration
   def change
     add_column :users, :birthday_month, :integer
     add_column :users, :birthday_day, :integer
-    remove_column :users, :date_of_birth
+    remove_column :users, :date_of_birth, :date
   end
 end
