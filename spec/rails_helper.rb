c4d71e833a64d87c80255175002dd2e40a6d74be
Ruto8 <pawel-rutkowski@hotmail.com>
2015-04-14 19:53:03 +0200

    Clean RSpec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,7 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.infer_spec_type_from_file_location!
 end
