c44688b9e53226ee70e73c26e899af5f00ad5c50
Ruto8 <pawel-rutkowski@hotmail.com>
2015-04-14 18:05:49 +0200

    Remove useless trailing slash

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsSso::Engine => '/sso', as: 'sso'
 
   namespace :api do
-    get 'users/', to: 'users#index'
+    get 'users', to: 'users#index'
     get 'users/me', to: 'users#edit'
     put 'users/me', to: 'users#update'
   end
