8c33070872acac013de6d1ca1967cc05411d01ca
Ruto8 <pawel-rutkowski@hotmail.com>
2015-05-02 12:04:12 +0200

    Proposition representer

--- app/controllers/api/propositions_controller.rb
+++ app/controllers/api/propositions_controller.rb
@@ -1,29 +1,24 @@
 module Api
   class PropositionsController < ApplicationController
 
-    def index
-      render json: Proposition.where(jubilat_id: params[:user_id]).order(updated_at: :asc)
-    end
-
     def create
-      render json: Proposition.create(proposition_params)
+      render json: PropositionRepresenter.new(Proposition.create(proposition_params)).basic
     end
 
     def update
       proposition = Proposition.find(params[:id])
-      proposition.update(proposition_params)
-      render json: proposition
-    end
-
-    def comments
-      proposition = Proposition.find(params[:id])
-
-      render json: proposition.comments
+      current_user = User.find_by(sso_id: current_user_data['uid'])
+      unless current_user['id'] != proposition.owner_id
+        proposition.update(proposition_params)
+        render json: PropositionRepresenter.new(proposition).basic
+      else
+        head :unauthorized
+      end
     end
 
   private
     def proposition_params
-      params.require(:proposition).permit(:jubilat_id, :description, :title, :value, :chosen).merge(owner_id: User.find_by(sso_id: current_user_data['uid']).id)
+      params.require(:proposition).permit(:jubilat_id, :description, :title, :value, :year_chosen_in).merge(owner_id: User.find_by(sso_id: current_user_data['uid']).id) #tu jest problem, nie przypisuje current usera do nowej propozycji
     end
   end
 end
---
+++ app/representers/proposition_representer.rb
@@ -0,0 +1,13 @@
+class PropositionRepresenter < Struct.new(:proposition)
+  def basic
+    {
+      id: proposition["id"],
+      title: proposition["title"],
+      description: proposition["description"],
+      value: proposition["value"],
+      year_chosen_in: proposition["year_chosen_in"],
+      owner: User.find(proposition["owner_id"]).attributes.slice("id", "name"),
+      comments: proposition.comments.map { |comment| CommentRepresenter.new(comment).basic }
+    }
+  end
+end
