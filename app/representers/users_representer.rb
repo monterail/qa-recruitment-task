e51871ccaf968c8a7323ba282006fd8ceaa70be3
Ruto8 <pawel-rutkowski@hotmail.com>
2015-05-05 19:24:38 +0200

    do-end instead of {} in non-inline block

--- app/representers/users_representer.rb
+++ app/representers/users_representer.rb
@@ -1,11 +1,11 @@
 class UsersRepresenter < Struct.new(:users)
   def basic
-    users.map { |user| {
+    users.map do |user| {
       name: user.name,
       id: user.id,
       birthday_day: user.birthday_day,
       birthday_month: user.birthday_month
       }
-    }
+    end
   end
 end
