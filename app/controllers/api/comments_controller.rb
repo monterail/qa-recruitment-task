8ed3caca4a1b1e2741418b4ff5f1246cf7ac2f28
Ruto8 <pawel-rutkowski@hotmail.com>
2015-05-08 13:15:39 +0200

    Restict_wrong_owner filter in propositions and comments controllers

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class CommentsController < ApplicationController
 
+    before_action :restrict_wrong_owner, only: [:update]
+
     def create
       proposition = Proposition.find(params[:proposition_id])
       new_comment = proposition.comments.create(comment_params)
@@ -9,18 +11,19 @@ module Api
 
     def update
       comment = Comment.find(params[:id])
-      if current_user['id'] == comment.owner_id
-        if comment.update(comment_params)
-          render json: CommentRepresenter.new(comment).basic
-        else
-          render json: { errors: comment.errors.messages }, status: 422
-        end
+      if comment.update(comment_params)
+        render json: CommentRepresenter.new(comment).basic
       else
-        head :unauthorized
+        render json: { errors: comment.errors.messages }, status: 422
       end
     end
 
   private
+    def restrict_wrong_owner
+      comment = Comment.find(params[:id])
+      head :unauthorized if current_user.id != comment.owner_id
+    end
+
     def comment_params
       params.require(:comment).permit(:proposition_id, :body).merge(owner_id: User.find_by(sso_id: current_user_data['uid']).id)
     end
--- app/controllers/api/propositions_controller.rb
+++ app/controllers/api/propositions_controller.rb
@@ -1,24 +1,27 @@
 module Api
   class PropositionsController < ApplicationController
 
+    before_action :restrict_wrong_owner, only: [:update]
+
     def create
       render json: PropositionRepresenter.new(current_user.propositions_as_owner.create(proposition_params)).basic
     end
 
     def update
       proposition = Proposition.find(params[:id])
-      if current_user['id'] == proposition.owner_id
-        if proposition.update(proposition_params)
-          render json: PropositionRepresenter.new(proposition).basic
-        else
-          render json: { errors: comment.errors.messages }, status: 422
-        end
+      if proposition.update(proposition_params)
+        render json: PropositionRepresenter.new(proposition).basic
       else
-        head :unauthorized
+        render json: { errors: comment.errors.messages }, status: 422
       end
     end
 
   private
+    def restrict_wrong_owner
+      proposition = Proposition.find(params[:id])
+      head :unauthorized if current_user.id != proposition.owner_id
+    end
+
     def proposition_params
       params.require(:proposition).permit(:jubilat_id, :description, :title, :value, :year_chosen_in)
     end
