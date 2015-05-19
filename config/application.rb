f0df85fbbc489a08b6221b9f243029eedd20c954
Ruto8 <ruto.voltage@gmail.com>
2015-05-19 16:31:12 +0200

    Merge assets.precompile +=

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ module BornHussaRs
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "vendor", "assets", "images")
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.precompile += ["cuirass-icons.eot", "cuirass-icons.svg", "cuirass-icons.ttf", "cuirass-icons.woff"]
+    config.assets.precompile += %w(
+      *.png *.jpg *.jpeg *.gif
+      cuirass-icons.eot cuirass-icons.svg cuirass-icons.ttf cuirass-icons.woff
+    )
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
