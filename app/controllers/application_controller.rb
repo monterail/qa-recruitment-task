511bd7aa5e07cf57b7376442e570496f3a3a8712
Ruto8 <ruto.voltage@gmail.com>
2015-05-24 12:42:09 +0200

    Move rescue_from above current_user in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   include JsEnv
   before_action :authenticate_user!
 
-  def current_user
-    @current_user ||= User.find_by(sso_id: current_user_data['uid'])
-  end
-
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: { message: e.message }, status: :not_found
+  end
+
+  def current_user
+    @current_user ||= User.find_by(sso_id: current_user_data['uid'])
   end
 end
