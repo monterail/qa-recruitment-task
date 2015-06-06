1e855d33f2e3df07289e42d8ecd93d4cad06247f
Ruto8 <ruto.voltage@gmail.com>
2015-06-06 09:56:46 +0200

    Validate person_responsible presence on Birthday

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -3,4 +3,5 @@ class Birthday < ActiveRecord::Base
   belongs_to :person_responsible, class_name: User, foreign_key: :person_responsible_id
 
   validates :celebrant, presence: true
+  validates :person_responsible, presence: true
 end
