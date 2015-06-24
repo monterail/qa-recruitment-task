01c5434f8a53e8a5533d317f6d71b47a208212bf
Ruto8 <ruto.voltage@gmail.com>
2015-06-24 11:07:28 +0200

    Move person responsible if chosen logic to front-end

--- app/representers/one_user_representer.rb
+++ app/representers/one_user_representer.rb
@@ -17,8 +17,6 @@ class OneUserRepresenter < Struct.new(:user)
         person_responsible: (
           if user.birthdays_as_celebrant.find_by_year(user.next_birthday_year)
             user.birthdays_as_celebrant.find_by_year(user.next_birthday_year).person_responsible.attributes.slice("id", "name", "email")
-          else
-            { name: 'Not chosen yet'}
           end
           ),
       profile_photo: "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}"
