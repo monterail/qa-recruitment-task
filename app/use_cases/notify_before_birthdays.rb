7f3f9db2dbcdefcb7254c91fd11fd041ff57745e
Ruto8 <ruto.voltage@gmail.com>
2015-07-06 15:05:32 +0200

    [BORN-6] Make birthday date mandatory when trying to send notifications before birthdays Before, we weren't checking if user has his birthday date set and when we tried to check days until his birthday, we got an error. This one more condition is fixing this, so we compare only users that have their birthday date set

--- app/use_cases/notify_before_birthdays.rb
+++ app/use_cases/notify_before_birthdays.rb
@@ -1,16 +1,19 @@
 class NotifyBeforeBirthdays
   def call
     when_to_notify = [1, 6, 15, 30]
-    User.ordered_by_soonest_birthday.each do |user|
-      if when_to_notify.include? days_till_birthday(user)
-        unless user.done
-          Notification.notify_before_birthdays(
-            days_till_birthday(user),
-            User.where.not(id: user.id),
-            user
-          ).deliver_now
+    User
+      .ordered_by_soonest_birthday
+      .where.not(birthday_month: nil, birthday_day: nil)
+      .each do |user|
+        if when_to_notify.include? days_till_birthday(user)
+          unless user.done
+            Notification.notify_before_birthdays(
+              days_till_birthday(user),
+              User.where.not(id: user.id),
+              user
+            ).deliver_now
+          end
         end
-      end
     end
   end
 
