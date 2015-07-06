2af2cb12a48e732fa379f7c076ccc0dd5e05d352
Ruto8 <ruto.voltage@gmail.com>
2015-07-06 15:00:22 +0200

    [BORN-6] Add specs for NotifyBeforeBirthdays use case

---
+++ spec/use_cases/notify_before_birthdays_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe NotifyBeforeBirthdays do
+  let(:dawid) { User.create(email: 'dawid@example.com', name: 'dawid', sso_id: '23456789') }
+  let!(:hodak) { User.create(email: 'hodak@example.com', name: 'hodak', sso_id: '23456790') }
+  let!(:jakub) { User.create(email: 'jakub@example.com', name: 'jakub', sso_id: '23456791') }
+
+  before(:each) do
+    ActionMailer::Base.deliveries = []
+  end
+
+  context "birthday in a month" do
+    let(:notify_date) { 30.days.from_now }
+
+    before(:each) do
+      dawid.update_attributes(
+        birthday_month: notify_date.month,
+        birthday_day: notify_date.day
+      )
+    end
+
+    it "sends email" do
+      expect { NotifyBeforeBirthdays.new.call }
+        .to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it "doesn't send email to celebrant" do
+      NotifyBeforeBirthdays.new.call
+      notification = ActionMailer::Base.deliveries.last
+      expect(notification.to).not_to include(dawid.email)
+    end
+
+    it "sends email to all users except celebrant" do
+      NotifyBeforeBirthdays.new.call
+      notification = ActionMailer::Base.deliveries.last
+      expect(notification.to).to include(hodak.email)
+      expect(notification.to).to include(jakub.email)
+    end
+
+    it "doesn't send email if birthday is done" do
+      dawid.update_attributes(done: true)
+      expect{ NotifyBeforeBirthdays.new.call }
+        .to change{ ActionMailer::Base.deliveries.count }.by(0)
+    end
+  end
+
+  it "sends emails when birthday is in the next year" do
+    time_december = Time.local(2015, 12, 15, 16, 37, 0)
+    Timecop.freeze(time_december)
+    notify_date = time_december + 30.days
+    dawid.update_attributes(
+      birthday_month: notify_date.month,
+      birthday_day: notify_date.day
+    )
+    expect{ NotifyBeforeBirthdays.new.call }
+      .to change{ ActionMailer::Base.deliveries.count }.by(1)
+    Timecop.return
+  end
+end
