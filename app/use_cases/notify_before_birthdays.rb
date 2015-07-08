a425ee2bf8b2da7bd92a1ed9f3f13e8ec459e1eb
Ruto8 <ruto.voltage@gmail.com>
2015-07-08 21:56:55 +0200

    [BORN-6] Add NotifyAboutBirthdays Worker As we've introduced sidekiq, I've moved notification-mailing logic to worker

--- app/use_cases/notify_before_birthdays.rb
+++ app/use_cases/notify_before_birthdays.rb
@@ -1,17 +1,13 @@
 class NotifyBeforeBirthdays
   def call
-    when_to_notify = [1, 6, 15, 30]
+    when_to_notify = [1, 5, 15, 30]
     User
       .ordered_by_soonest_birthday
       .where.not(birthday_month: nil, birthday_day: nil)
       .each do |user|
         if when_to_notify.include? days_till_birthday(user)
           unless user.done
-            Notification.notify_before_birthdays(
-              days_till_birthday(user),
-              User.where.not(id: user.id),
-              user
-            ).deliver_now
+            NotifyAboutBirthdaysWorker.perform_async(user.id)
           end
         end
     end
