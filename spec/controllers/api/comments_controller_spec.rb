83ef131f39e64b085a30543aab74e97b98b3636c
Ruto8 <ruto.voltage@gmail.com>
2015-07-16 12:46:45 +0200

    [style] split lines with too many characters

--- spec/controllers/api/comments_controller_spec.rb
+++ spec/controllers/api/comments_controller_spec.rb
@@ -70,7 +70,8 @@ describe Api::CommentsController do
 
     context "if owner is current_user" do
       it "deletes comment" do
-        delete :destroy, proposition_id: comment_attributes['proposition_id'], id: comment_attributes['id']
+        delete :destroy, proposition_id: comment_attributes['proposition_id'],
+          id: comment_attributes['id']
         expect(response.status).to eq(200)
         expect(Comment.find_by(id: comment_attributes['id'])).not_to eq(true)
       end
@@ -79,7 +80,8 @@ describe Api::CommentsController do
     context "if owner isn't current_user" do
       it "return unauthorized" do
         auth(User.create!(name: 'baduser', email: 'bad@user.eu', sso_id: '87654321'))
-        delete :destroy, proposition_id: comment_attributes['proposition_id'], id: comment_attributes['id']
+        delete :destroy, proposition_id: comment_attributes['proposition_id'],
+          id: comment_attributes['id']
         expect(response.status).to eq(401)
       end
     end
