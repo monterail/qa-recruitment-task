6c69d91c77d1f2e65ad734ae69e7ca74ea08777e
Magdalena Poprawa <magdalena.poprawa@monterail.com>
2015-08-27 15:34:24 +0200

    [BORN-60] Change ENVied to Env in application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module BornHussaRs
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.secret_key_base = ENVied.SECRET_KEY_BASE
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
     config.assets.paths << Rails.root.join("app", "vendor", "assets", "images")
     config.assets.precompile += %w(
       *.png *.jpg *.jpeg *.gif
