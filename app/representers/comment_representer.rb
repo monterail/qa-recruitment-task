4b46275b474f49e73fe9588a7744f6b502f222dc
Daniel <daniel.montenegro@monterail.com>
2015-12-17 16:30:52 +0100

    Allow retired users to see their comments
      Remove the participating filter to the representer of proposition and
      comments

--- app/representers/comment_representer.rb
+++ app/representers/comment_representer.rb
@@ -3,7 +3,7 @@ CommentRepresenter = Struct.new(:comment) do
     {
       id: comment.id,
       body: comment.body,
-      owner: User.participating.find(comment.owner_id).attributes.slice("id", "name"),
+      owner: User.find(comment.owner_id).attributes.slice("id", "name"),
       created_at: comment.created_at,
     }
   end
