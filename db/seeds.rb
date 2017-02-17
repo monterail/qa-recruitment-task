d127e1e4a911bc813375e75da96816b03fe4ee36
brtkeee <mendrela.bartek@gmail.com>
2017-02-17 12:11:00 +0100

    Add new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.create(email: "qa1@monterail.com", birthday_month: 1, birthday_day: 4 ,name: "Long John",sso_id: "qa1ssoid" , password: "123456")
-User.create(email: "qa2@monterail.com", birthday_month: 2, birthday_day: 4 ,name: "Small John",sso_id: "qa2ssoid" , password: "123456")
-User.create(email: "qa3@monterail.com", birthday_month: 3, birthday_day: 4 ,name: "Pink John",sso_id: "qa3ssoid" , password: "123456")
-User.create(email: "qa4@monterail.com", birthday_month: 4, birthday_day: 9 ,name: "Red John",sso_id: "qa4ssoid" , password: "123456")
-User.create(email: "qa5@monterail.com", birthday_month: 5, birthday_day: 9 ,name: "Black John",sso_id: "qa5ssoid" , password: "123456")
-User.create(email: "qa6@monterail.com", birthday_month: 6, birthday_day: 9 ,name: "Green John",sso_id: "qa6ssoid" , password: "123456")
+User.create(email: "qa1@monterail.com", birthday_month: 1, birthday_day: 4, name: "Long John",sso_id: "qa1ssoid" , password: "123456")
+User.create(email: "qa2@monterail.com", birthday_month: 2, birthday_day: 4, name: "Small John",sso_id: "qa2ssoid" , password: "123456")
+User.create(email: "qa3@monterail.com", birthday_month: 3, birthday_day: 4, name: "Pink John",sso_id: "qa3ssoid" , password: "123456")
+User.create(email: "qa4@monterail.com", birthday_month: 4, birthday_day: 9, name: "Red John",sso_id: "qa4ssoid" , password: "123456")
+User.create(email: "qa5@monterail.com", birthday_month: 5, birthday_day: 9, name: "Black John",sso_id: "qa5ssoid" , password: "123456")
+User.create(email: "qa6@monterail.com", birthday_month: 6, birthday_day: 9, name: "Green John",sso_id: "qa6ssoid" , password: "123456")
+User.create(email: "qa7@monterail.com", birthday_month: 6, birthday_day: 9, name: "Blue John",sso_id: "qa7ssoid" , password: "123456")
+User.create(email: "qa8@monterail.com", birthday_month: 6, birthday_day: 9, name: "Purple John",sso_id: "qa8ssoid" , password: "123456")
